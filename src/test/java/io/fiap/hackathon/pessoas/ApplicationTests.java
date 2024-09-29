d74b4d80ee19399c50f7596a84ba6e87544ab0f1
Icaro Damiani <icaro.damiani1@vodafone.com>
2024-09-28 21:32:24 -0300

    feat: CI.

--- src/test/java/io/fiap/hackathon/pessoas/ApplicationTests.java
+++ src/test/java/io/fiap/hackathon/pessoas/ApplicationTests.java
@@ -2,12 +2,14 @@ package io.fiap.hackathon.pessoas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(value = "local")
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
